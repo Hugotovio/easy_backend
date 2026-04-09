6e74b1522cff2302575241bb0046fecf5cb8cad5
Tovy7 <gohutb@hotmail.com>
2026-04-09 12:29:40 -0500

    se habilitan todos los cors

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from routers import liquidacion
+from fastapi.middleware.cors import CORSMiddleware
  
 app = FastAPI()
-app.include_router(liquidacion.router, prefix="/api/v1", tags=["liquidacion"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=
+    
+        ["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(liquidacion.router, prefix="/api/v1")
