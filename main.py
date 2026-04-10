10b46ec64ef753b7c61dd1dd975cb1fd479803b2
Tovy7 <gohutb@hotmail.com>
2026-04-09 21:23:10 -0500

    ajuste de cors

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.middleware.proxy_headers import ProxyHeadersMiddleware
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 app = FastAPI()
 
-# 👇 CLAVE
-app.add_middleware(ProxyHeadersMiddleware)
-
-# 👇 opcional pero recomendado
-app.add_middleware(HTTPSRedirectMiddleware)
 
 # 👇 tu CORS
 app.add_middleware(
