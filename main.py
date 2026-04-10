c9ba2ee324344a0eac96253ac04223610f38d4e0
Tovy7 <gohutb@hotmail.com>
2026-04-09 20:55:43 -0500

    configuracion de cors

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://beneficial-courtesy-desarrollo.up.railway.app"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
